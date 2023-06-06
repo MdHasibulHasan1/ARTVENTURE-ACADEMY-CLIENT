666d04f09be0cece9e75d526b603f2a4fca9fd69
hasib1511 <hasib7143@gmail.com>
2023-06-06 22:16:01 +0600

    create sign up page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 
 import Home from "../Pages/Home/Home";
+import SignUp from "../pages/SignUP/SignUp";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
       },
     ],
   },
